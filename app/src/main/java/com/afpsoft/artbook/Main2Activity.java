8fc556ba80a9fee4d373c92689818d4d956f7bac
alipiskin <alipiskin@hotmail.com>
2018-02-21 16:41:59 +0300

    -

--- app/src/main/java/com/afpsoft/artbook/Main2Activity.java
+++ app/src/main/java/com/afpsoft/artbook/Main2Activity.java
@@ -1,19 +1,33 @@
 package com.afpsoft.artbook;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 public class Main2Activity extends AppCompatActivity {
 
     ImageView imageView;
+    EditText editText;
+    Button buton;
+    static SQLiteDatabase database;
+    Bitmap selectedImage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +35,54 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         imageView= findViewById(R.id.imageView);
+        editText= findViewById(R.id.editText);
+        buton= findViewById(R.id.save);
+
+        Intent intent= getIntent();
+        String info=intent.getStringExtra("info");
+
+        if (info.equalsIgnoreCase("new")){
+
+            Bitmap background= BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.select);
+
+            imageView.setImageBitmap(background);
+
+            buton.setVisibility(View.VISIBLE);
+            editText.setText("");
+
+        }else {
+
+            buton.setVisibility(View.INVISIBLE);
+
+        }
     }
 
     public void select(View view){
 
-        Intent intent= new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(intent,1);
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},2);
+
+        } else {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, 1);
+        }
 
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode==2) {
+
+            if ( grantResults.length > 0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 1);
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     @Override
@@ -39,8 +93,8 @@ public class Main2Activity extends AppCompatActivity {
             Uri image = data.getData();
 
             try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), image);
-                imageView.setImageBitmap(bitmap);
+                selectedImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), image);
+                imageView.setImageBitmap(selectedImage);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -50,6 +104,31 @@ public class Main2Activity extends AppCompatActivity {
     }
 
     public void save (View view){
+
+        String artName=editText.getText().toString();
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        selectedImage.compress(Bitmap.CompressFormat.PNG,50,outputStream);
+        byte[] byteArray=outputStream.toByteArray();
+
+        try {
+
+            database=this.openOrCreateDatabase("Arts",MODE_PRIVATE,null);
+            database.execSQL("CREATE TABLE IF NOT EXISTS arts (name VARCHAR, image BLOB)");
+
+            String sqlString="INSERT INTO arts (name,image) VALUES (?, ?)";
+            SQLiteStatement statement= database.compileStatement(sqlString);
+            statement.bindString(1,artName);
+            statement.bindBlob(2,byteArray);
+            statement.execute();
+
+        }catch(Exception e) {
+           e.printStackTrace();
+        }
+
+        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(intent);
+
 
 
     }
--- app/src/main/java/com/afpsoft/artbook/MainActivity.java
+++ app/src/main/java/com/afpsoft/artbook/MainActivity.java
@@ -1,12 +1,18 @@
 package com.afpsoft.artbook;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         if(item.getItemId()==R.id.add_art){
 
             Intent intent=new Intent(getApplicationContext(),Main2Activity.class);
+            intent.putExtra("info","new");
             startActivity(intent);
 
 
@@ -39,6 +46,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ListView listview= findViewById(R.id.listView);
+
+        ArrayList<String > artName= new ArrayList<>();
+        ArrayList<Bitmap> artImage=new ArrayList<>();
+
+        ArrayAdapter arrayAdapter= new ArrayAdapter(this,android.R.layout.simple_list_item_1,artName);
+        listview.setAdapter(arrayAdapter);
+
+        try {
+
+            Main2Activity.database=this.openOrCreateDatabase("Arts",MODE_PRIVATE,null);
+            Main2Activity.database.execSQL("CREATE TABLE IF NOT EXISTS arts (name VARCHAR,image BLOB)");
+
+            Cursor cursor = Main2Activity.database.rawQuery("SELECT * FROM arts", null);
+
+            int nameIx= cursor.getColumnIndex("name");
+            int ImageIx=cursor.getColumnIndex("image");
+
+            while (cursor!=null) {
+
+                System.out.println("nameIX şudur ki" + nameIx);
+                artName.add(cursor.getString(nameIx));
+
+                byte [] byteArray= cursor.getBlob(ImageIx);
+                Bitmap image= BitmapFactory.decodeByteArray(byteArray,0,byteArray.length);
+                artImage.add(image);
+
+                cursor.moveToNext();
+
+                arrayAdapter.notifyDataSetChanged();
+
+
+            }
+
+        }catch (Exception e) {
+
+            e.printStackTrace();
+        }
+
 
 
     }
